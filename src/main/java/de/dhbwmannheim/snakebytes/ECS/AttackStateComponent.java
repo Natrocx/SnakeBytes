d5998b14fa63eb49c1b41ea366576d8ee96893f9
kngkiro <kirolis2002@gmail.com>
2021-12-12 13:30:32 +0100

    Framehandler updated, integrated into Engine

--- src/main/java/de/dhbwmannheim/snakebytes/ECS/AttackStateComponent.java
+++ src/main/java/de/dhbwmannheim/snakebytes/ECS/AttackStateComponent.java
@@ -6,8 +6,7 @@ public class AttackStateComponent extends Component {
     public int id = 0b100000000;
     public int state; //0=Pointer links 1= Pointer rechts 2= Buch links 3= buch rechts
 
-    public AttackStateComponent(int id, int state) {
-        this.id = id;
+    public AttackStateComponent(int state) {
         this.state = state;
     }
 
